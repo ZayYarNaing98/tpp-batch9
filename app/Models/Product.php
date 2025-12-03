0cd335edb9d390edfe40b0b02836985705fb955e
ZayYarNaing <zayarnaing.pp@gmail.com>
2025-12-03 16:30:34 +0630

    Add Product Category relation

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,12 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Product extends Model
 {
     protected $fillable = [
+        'category_id',
         'name',
         'description',
         'price',
     ];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class, 'category_id');
+    }
 }
